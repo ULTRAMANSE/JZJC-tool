f4a322f4db75bb69d5e10590be31eb39244cf797
ULTRAMANSE <1076012962@qq.com>
2020-07-26 14:18:51 +0800

    更新

--- UIW.py
+++ UIW.py
@@ -5,14 +5,12 @@ from PyQt5.Qt import QThread, QMutex
 from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from docx import Document
 import copy
-import xlrd
-import time
 
 
 class WordU(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(WordU, self).__init__(parent)
-        self.setFixedSize(400, 200)
+        self.setFixedSize(400, 150)
         self.setWindowTitle("自动填写")
         self.file_path = None
         self.save_word = None
@@ -30,36 +28,30 @@ class WordU(QtWidgets.QWidget):
         self.activity()
     
     def activity(self):
-        self.file_in.clicked.connect(self.choose_x_file)
         self.save_in.clicked.connect(self.choose_w_file)
         self.yes_b.clicked.connect(self.start_W)
     
     def set_prom(self):
-        self.file_path = QLineEdit(self)
-        self.file_in = QPushButton("选择Excel", self)
-        self.file_path.setReadOnly(True)
-        self.glayout.addWidget(self.file_path, 1, 1, 1, 10)
-        self.glayout.addWidget(self.file_in, 1, 11, 1, 4)
         
         self.save_word = QLineEdit(self)
         self.save_in = QPushButton("选择Word", self)
         self.save_word.setReadOnly(True)
-        self.glayout.addWidget(self.save_word, 2, 1, 1, 10)
-        self.glayout.addWidget(self.save_in, 2, 11, 1, 4)
+        self.glayout.addWidget(self.save_word, 1, 1, 1, 10)
+        self.glayout.addWidget(self.save_in, 1, 11, 1, 4)
         
         self.style_in = QComboBox(self)
         self.style_in.addItems(["测试说明", "测试记录"])
-        self.glayout.addWidget(self.style_in, 3, 4, 1, 2)
+        self.glayout.addWidget(self.style_in, 2, 4, 1, 2)
         self.yes_b = QPushButton("开始填写", self)
-        self.glayout.addWidget(self.yes_b, 3, 8, 1, 4)
+        self.glayout.addWidget(self.yes_b, 2, 8, 1, 4)
         self.prompt = QLabel(self)
-        self.glayout.addWidget(self.prompt, 3, 1, 1, 2)
-    
-    def choose_x_file(self):
-        filename, i = QFileDialog.getOpenFileNames(None, "请选择要添加的文件", "./",
-                                                   "Text Files (*.xlsx);;Text Files (*.xls);;All Files (*)")
-        if filename:
-            self.file_path.setText(filename[0])
+        self.glayout.addWidget(self.prompt, 2, 1, 1, 2)
+
+    # def choose_x_file(self):
+        # filename, i = QFileDialog.getOpenFileNames(None, "请选择要添加的文件", "./",
+        #                                            "Text Files (*.xlsx);;Text Files (*.xls);;All Files (*)")
+        # if filename:
+        #     self.file_path.setText(filename[0])
     
     def choose_w_file(self):
         filename, i = QFileDialog.getOpenFileNames(None, "请选择要添加的文件", "./",
@@ -70,12 +62,9 @@ class WordU(QtWidgets.QWidget):
     def start_W(self):
         self.prompt.setText("执行中...")
         in_item = self.style_in.currentIndex()
-        # if in_item == 1:
-        #     in_item = 2
-        self.write_word = ThreadRW(self.file_path.text(), self.save_word.text(), in_item)
+        self.write_word = ThreadRW(self.save_word.text(), in_item)
         self.write_word.str_out.connect(self.prompt_out)
         self.write_word.start()
-        # time.sleep(1)
     
     @pyqtSlot(str)
     def prompt_out(self, i):
@@ -88,16 +77,15 @@ lock = QMutex()
 class ThreadRW(QThread):
     str_out = pyqtSignal(str)
     
-    def __init__(self, xfile=None, wfile=None, item=None, parent=None):
+    def __init__(self, wfile=None, item=None, parent=None):
         super().__init__(parent)
-        self.R_file = xfile
-        self.W_file = wfile
-        self.Item = item
-        self.number = 0
-        self.num = 0
-        self.t_number = 5
-        self.t_number_copy = 0
-        self.t_rows = 1
+        self.W_file = wfile # word文件名
+        self.Item = item # 类型选项
+        self.number = 0 # 记录差
+        self.num = 0 # 列表下表值
+        self.t_number = 5 # 表格index
+        self.t_number_copy = 0  # 复制表格index
+        self.t_rows = 1 # 表格的行index
     
     def move_table_after(self, table, paragraph):
         tbl, p = table._tbl, paragraph._p
@@ -157,9 +145,7 @@ class ThreadRW(QThread):
             if self.t_number_copy == 0:
                 break
             copy_tb = copy.deepcopy(tb[self.t_number_copy])
-            # tb[l].cell(0, 1).text = temp[num][0]
             tb[self.t_number_copy].cell(0, 1).text = example_name[self.num]
-            # run = tb[l].cell(0, 5 + self.number).paragraphs[0].add_run(temp[num][1])
             run = tb[self.t_number_copy].cell(0, 5 + self.number).paragraphs[0].add_run(identity[self.num])
             run.font.name = 'Times New Roman'
             self.t_number_copy += 1
